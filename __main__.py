2024791cdd1fd2cab99bf1c9aa13dd8b825130ba
Maxim Andreev <andrianov_maksim@outlook.com>
2024-12-24 16:59:10 +0300

    All tests completed successfully, code coverage is 100%

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
 import src.widget, src.processing
 
 if __name__ == "__main__":
-    print(src.widget.mask_account_card())
-    print(src.widget.mask_account_card())
+    # print(src.widget.mask_account_card())
+    # print(src.widget.mask_account_card())
 
-    print(src.processing.filter_by_state())
-    print(src.processing.sort_by_date())
+    # print(src.processing.filter_by_state())
+    print(src.processing.sort_by_date([
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-13T08:21:33.419441"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-13T08:20:00.419441"}
+    ], False))
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,4 @@
 import pytest
-import sys
-import os
-
 
 @pytest.fixture
 def card_number():
@@ -22,37 +19,31 @@ def input_filter_by_state():
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
 
-@pytest.fixture()
-def output_filter_by_state_EXECUTED():
-    return [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-    ]
-
-@pytest.fixture()
-def output_filter_by_state_CANCELED():
-    return [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]
-
 @pytest.fixture
 def list_data_bank_operation():
-    data = [
+    return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-13T08:21:33.419441"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-13T08:20:00.419441"}
     ]
-    return data
 
 @pytest.fixture
-def output_list_data_bank_operation():
-    data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+def output_list_data_bank_operation_true():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-13T08:21:33.419441'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-13T08:20:00.419441'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
     ]
-    return data
+
+
+@pytest.fixture
+def output_list_data_bank_operation_false():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, 
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, 
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-13T08:20:00.419441'}, 
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-13T08:21:33.419441'}, 
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+    ]
