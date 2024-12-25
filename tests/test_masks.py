dbc5bb085e8c4db28aca046d9745f02f99f9ecda
Maxim Andreev <andrianov_maksim@outlook.com>
2024-12-25 09:12:06 +0300

    config mypy do not apply

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,9 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_card_number(card_number) -> str:
+def test_get_mask_card_number(card_number):
     assert get_mask_card_number(card_number, 14) == "Visa Platinum 7000 79** **** 6361 "
 
 
-def test_get_mask_account(account_number) -> str:
+def test_get_mask_account(account_number):
     assert get_mask_account(account_number, 5) == "Счет **4305"
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -13,17 +13,17 @@ from src.processing import filter_by_state, sort_by_date
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]),
 ])
-def test_proc_filter_by_state(input_filter_by_state, key, expected) -> list:
+def test_proc_filter_by_state(input_filter_by_state, key, expected):
     assert filter_by_state(input_filter_by_state, key) == expected
 
 
 def test_proc_sort_by_date_true(
     list_data_bank_operation: list = [], output_list_data_bank_operation_true: list = []
-) -> list:
+):
     assert sort_by_date(list_data_bank_operation, True) == output_list_data_bank_operation_true
 
 
 def test_proc_sort_by_date_false(
     list_data_bank_operation: list = [], output_list_data_bank_operation_false: list = []
-) -> list:
+):
     assert sort_by_date(list_data_bank_operation, False) == output_list_data_bank_operation_false
