f3672ff4c1c66f1825e6167e59e2c2a0a1869e86
Maxim Andreev <andrianov_maksim@outlook.com>
2025-01-27 10:00:19 +0300

    not god

--- src/executive.py
+++ src/executive.py
@@ -13,20 +13,26 @@ def executive_function_output_main(
 ):
     if user_choice_date != "да" and user_choice_date != "нет":
         user_choice_date = "нет"
-    elif user_choice_rub != "да" and user_choice_rub != "нет":
-        user_choice_rub = "нет"
-    elif user_choice_name_discription != "да" and user_choice_name_discription != "нет":
+    elif user_choice_sort == "да":
+        if user_choice_sort == "по возрастанию":
+            user_choice_sort = False
+        else:
+            user_choice_sort = True
+    
+    if user_choice_rub != "по возрастанию" and user_choice_rub != "по убыванию":
+        user_choice_rub = True
+    
+    
+    if user_choice_sort != "по возрастанию" and user_choice_sort != "по убыванию":
+        user_choice_sort = True
+        
+    if user_choice_name_discription != "да" and user_choice_name_discription != "нет":
         user_choice_name_discription = "нет"
 
-    elif user_choice_sort != "по возрастанию" and user_choice_sort != "по убыванию":
-        user_choice_sort = True
-    elif user_choice_sort == "по убыванию":
-        user_choice_sort = True
-    else:
-        user_choice_sort = False
+    
 
     if file_path == "data/operations.json" or file_path == "data/transactions_excel.xlsx":
-        return sort_by_date(filter_by_state(function_choice(file_path), user_key), user_choice_sort)
+        return sort_by_date(filter_by_state(function_choice(file_path), user_key), user_choice_date)
     else:
         return function_choice(file_path)
 
