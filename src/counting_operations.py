a58fa6fd200812fe2cafca2707138f67e0ac6f04
Maksim Andreev <andrianov_maksim@outlook.com>
2025-02-01 10:45:21 +0300

    add couting list operation, function counter

--- src/counting_operations.py
+++ src/counting_operations.py
@@ -1,5 +1,13 @@
 
 
 
-def counting_operations(list_opera):
-    pass
+type_operation_list = [
+    "Перевод со счета на счет",
+    "Перевод с карты на карту",
+    "Перевод организации",
+    "Открытие вклада"
+]
+
+def counting_operations(list_operation: list[dict] = [], operatin_type: list = []) -> dict:
+    for i, operat in enumerate(list_operation):
+        pass
